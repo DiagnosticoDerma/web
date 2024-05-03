093da6017f52ae132a413eb9ca65ef933da156ef
DiagnosticoDerma <nizamagi@ucvvirtual.edu.pe>
2024-05-03 09:00:49 -0500

    Delete resources directory

--- resources/views/email/diagnostico.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	<p>Estimado {{ $paciente->nombres." ".$paciente->apellidos }} le hacemos llegar sus resultados de su diagnóstico.</p>
-</body>
-</html>
